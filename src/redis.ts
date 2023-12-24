fd33f9e01cef92379dad54c335ea2577228b3501
billychen0894 <billychen0894@gmail.com>
2023-12-23 23:02:24 -0800

    add: Redis module

---
+++ src/redis.ts
@@ -0,0 +1,32 @@
+import { RedisClientType, createClient } from '@redis/client';
+
+export class RedisClient {
+  private static instance: RedisClient;
+  private client: RedisClientType;
+
+  private constructor() {
+    this.client = createClient();
+
+    this.client.on('connection', () => {
+      console.log('🚀 Connected to Redis');
+    });
+
+    this.client.on('error', err => {
+      console.error('Redis Error: ', err);
+    });
+
+    this.client.connect();
+  }
+
+  public static getInstance(): RedisClient {
+    if (!RedisClient.instance) {
+      RedisClient.instance = new RedisClient();
+    }
+
+    return RedisClient.instance;
+  }
+
+  public getClient(): RedisClientType {
+    return this.client;
+  }
+}
