34566bc8043e277d743d6df8406cec5b521cd1ef
billychen0894 <billychen0894@gmail.com>
2024-01-18 20:09:19 -0800

    add: implement clustering

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,37 @@
 import { App } from '@/app';
 import { ChatRoute } from '@/routes/chats.route';
+import { setupPrimary } from '@socket.io/cluster-adapter';
 import { ValidateEnv } from '@utils/validateEnv';
 import { ChatSocket } from '@websocket/chat.socket';
 import { Websocket } from '@websocket/websocket';
+import cluster from 'cluster';
+import { availableParallelism } from 'os';
 
 ValidateEnv();
 
-const app = new App([new ChatRoute()]);
+if (cluster.isPrimary) {
+  const numCPUs = availableParallelism();
+  // create one worker per available core
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork({
+      PORT: 4040 + i,
+    });
+  }
 
-const httpServer = app.getHttpServer();
-const io = Websocket.getWebsocket(httpServer);
+  // set up the adapter on the primary thread
+  setupPrimary();
+} else {
+  const app = new App([new ChatRoute()]);
 
-io.initializeHandlers([
-  {
-    path: '/chat',
-    handler: new ChatSocket(),
-  },
-]);
+  const httpServer = app.getHttpServer();
+  const io = Websocket.getWebsocket(httpServer);
 
-app.listen();
+  io.initializeHandlers([
+    {
+      path: '/chat',
+      handler: new ChatSocket(),
+    },
+  ]);
+
+  app.listen();
+}
--- src/websocket/websocket.ts
+++ src/websocket/websocket.ts
@@ -1,5 +1,6 @@
 import { SOCKET_ORIGIN } from '@/config';
 import { SocketInterface } from '@/interfaces/sockets.interface';
+import { createAdapter } from '@socket.io/cluster-adapter';
 import { Server, Socket } from 'socket.io';
 
 export class Websocket extends Server {
@@ -12,6 +13,8 @@ export class Websocket extends Server {
         methods: ['POST', 'GET'],
       },
       connectionStateRecovery: {},
+      // set up the adapter on each worker thread
+      adapter: createAdapter(),
     });
   }
 
