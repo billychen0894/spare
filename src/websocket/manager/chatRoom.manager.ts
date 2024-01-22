1e2b709547bd879688d6741dff0fc1031c9762b5
billychen0894 <billychen0894@gmail.com>
2024-01-21 20:44:02 -0800

    update: refactor chatRoomManager

--- src/websocket/manager/chatRoom.manager.ts
+++ src/websocket/manager/chatRoom.manager.ts
@@ -1,23 +1,23 @@
 import { ChatMessage, CustomSocket } from '@/interfaces/sockets.interface';
 import { RedisService } from '@/services/redis.service';
-import { Socket } from 'socket.io';
+import { Server as IOServer } from 'socket.io';
 import Container from 'typedi';
 
 export class ChatRoomManager {
   private redisService: RedisService;
-  private userSockets: Map<string, CustomSocket>;
+  private io: IOServer;
 
-  constructor() {
+  constructor(io: IOServer) {
     this.redisService = Container.get(RedisService);
-    this.userSockets = new Map<string, Socket>();
+    this.io = io;
 
     // check inactivity of chatRooms in every one hour
     setInterval(
       () => {
         this.checkInactiveChatRooms().catch(console.error);
       },
-      60 * 60 * 1000,
-      // 30 * 1000,
+      // 60 * 60 * 1000,
+      30 * 1000,
     );
   }
 
@@ -25,10 +25,6 @@ export class ChatRoomManager {
     const isEventProcessed = await this.redisService.processSocketEvent(event, eventId);
 
     if (isEventProcessed) return;
-
-    if (socket.sessionId && !this.userSockets.has(socket.sessionId)) {
-      this.userSockets.set(userId, socket);
-    }
 
     // Check user session if it's already existed in Redis
     if (socket.sessionId && socket.chatRoomId) {
@@ -40,18 +36,8 @@ export class ChatRoomManager {
         const isUserInRoom = participants.includes(socket.sessionId);
 
         if (userStatus === 'in-chat' && isUserInRoom) {
-          const otherPairedUserId = participants.find(id => id !== socket.sessionId);
-
           socket.join(socket.chatRoomId);
-
-          if (otherPairedUserId) {
-            // Get the other user socket in order to emit chatRoom connected event to the other user that has session
-            const otherPairedUserSocket = this.userSockets.get(otherPairedUserId);
-
-            otherPairedUserSocket
-              ?.to(socket.chatRoomId)
-              .emit('chatRoom-created', { id: socket.chatRoomId, state: chatRoom.state, participants: chatRoom.participants });
-          }
+          socket.emit('chatRoom-created', { id: socket.chatRoomId, state: chatRoom.state, participants: chatRoom.participants });
 
           callback();
           return;
@@ -60,45 +46,41 @@ export class ChatRoomManager {
     }
 
     await this.redisService.addUserToQueue(userId);
-    const isPaired = await this.redisService.pairUsers();
+    const chatRoom = await this.redisService.pairUsers();
 
-    if (isPaired) {
-      const otherPairedUserId = Array.from(isPaired.participants).find(id => id !== userId);
+    if (chatRoom) {
+      const otherPairedUserId = Array.from(chatRoom.participants).find(id => id !== userId);
 
       // If it's paired, update chatRoomId in session obj
-      socket.emit('session', { sessionId: socket.sessionId, chatRoomId: isPaired.id });
+      socket.emit('session', { sessionId: socket.sessionId, chatRoomId: chatRoom.id });
+      socket.join(chatRoom.id);
+      socket.emit('chatRoom-created', chatRoom);
 
       if (otherPairedUserId) {
-        const otherPairedSocket = this.userSockets.get(otherPairedUserId);
-
-        socket.join(isPaired.id);
-        otherPairedSocket?.join(isPaired.id);
-        otherPairedSocket?.emit('session', { sessionId: otherPairedSocket.sessionId, chatRoomId: isPaired.id });
-
-        socket.to(isPaired.id).emit('chatRoom-created', isPaired);
-        otherPairedSocket?.to(isPaired.id).emit('chatRoom-created', isPaired);
+        this.io?.of('/chat').in(otherPairedUserId).socketsJoin(chatRoom.id);
+        this.io?.of('/chat').to(otherPairedUserId).emit('session', { sessionId: otherPairedUserId, chatRoomId: chatRoom.id });
+        this.io?.of('/chat').to(otherPairedUserId).emit('chatRoom-created', chatRoom);
       }
     }
     callback();
   }
 
-  public async leaveChatRoom(socket: CustomSocket, chatRoomId: string, callback: any): Promise<void> {
+  // TODO: On disconnect the previous socket events are recovered with new socket, something to do with acknowledgement
+  public async leaveChatRoom(event: string, socket: CustomSocket, chatRoomId: string, eventId: string, callback: any): Promise<void> {
+    const isEventProcessed = await this.redisService.processSocketEvent(event, eventId);
+
+    if (isEventProcessed) return;
+
     if (chatRoomId) {
       const socketId = socket.sessionId ? socket.sessionId : socket.id;
 
       socket.to(chatRoomId).emit('left-chat', socketId);
       socket.leave(chatRoomId);
-      this.userSockets.delete(socketId);
 
-      const userKey = `user:${socketId}:lastActivity`;
-      const chatRoomKey = `chatRoom:${chatRoomId}:lastActivity`;
-      await this.redisService.removeUserMessageIds(socketId, chatRoomId);
-      await this.redisService.leaveChatRoomById(chatRoomId, socketId);
-      await this.redisService.deleteChatRoomMessagesById(chatRoomId);
-      await this.redisService.deleteLastActiveTime(userKey, socketId);
-      await this.redisService.deleteLastActiveTime(chatRoomKey, socketId);
+      this.redisService.clearUser(socketId, chatRoomId);
+    } else {
+      this.redisService.removeUserSessionId(socket.sessionId || socket.id);
     }
-
     callback();
   }
 
@@ -131,11 +113,7 @@ export class ChatRoomManager {
         const chatMessages = await this.redisService.retrieveMessages(chatRoomId);
 
         if (chatMessages) {
-          const socketUserId = socket.sessionId ? socket.sessionId : socket.id;
-          const originalSocket = this.userSockets.get(socketUserId);
-
-          // use original socket to broadcast event to chatRoom as the socket itself needs to be notified in order to get chat history
-          originalSocket?.to(chatRoomId).emit('chat-history', chatMessages);
+          this.io?.of('/chat').to(chatRoomId).emit('chat-history', chatMessages);
         }
 
         callback();
@@ -174,42 +152,23 @@ export class ChatRoomManager {
 
       if (chatRooms && chatRooms?.length > 0) {
         for (const chatRoom of chatRooms) {
-          const thresholdInSeconds = 2 * 24 * 60 * 60; // two days;
-          // const thresholdInSeconds = 30; // 30 sec;
+          // const thresholdInSeconds = 2 * 24 * 60 * 60; // two days;
+          const thresholdInSeconds = 30; // 30 sec;
           const isInactive = await this.redisService.isInactive(`chatRoom:${chatRoom?.id}:lastActivity`, thresholdInSeconds);
 
           if (isInactive) {
-            // Broadcast to chatroom about inactivity and the chat room is removed
-            const user1Socket = this.userSockets.get(chatRoom.participants[0]);
-            const user2Socket = this.userSockets.get(chatRoom.participants[1]);
+            chatRoom.participants.forEach(async participant => {
+              const userSessionId = await this.redisService.getUserSessionId(participant);
 
-            if (user1Socket) {
-              user1Socket.to(chatRoom?.id).emit('inactive-chatRoom', chatRoom);
+              if (userSessionId) {
+                this.redisService.clearUser(userSessionId, chatRoom?.id);
+              }
+            });
 
-              const socketId = user1Socket.sessionId ? user1Socket.sessionId : user1Socket.id;
-              const chatRoomId = chatRoom.id;
-              const userKey = `user:${socketId}:lastActivity`;
-              const chatRoomKey = `chatRoom:${chatRoomId}:lastActivity`;
-              await this.redisService.removeUserMessageIds(socketId, chatRoomId);
-              await this.redisService.leaveChatRoomById(chatRoomId, socketId);
-              await this.redisService.deleteChatRoomMessagesById(chatRoomId);
-              await this.redisService.deleteLastActiveTime(userKey, socketId);
-              await this.redisService.deleteLastActiveTime(chatRoomKey, socketId);
-            }
-
-            if (user2Socket) {
-              user2Socket.to(chatRoom?.id).emit('inactive-chatRoom', chatRoom);
-
-              const socketId = user2Socket.sessionId ? user2Socket.sessionId : user2Socket.id;
-              const chatRoomId = chatRoom.id;
-              const userKey = `user:${socketId}:lastActivity`;
-              const chatRoomKey = `chatRoom:${chatRoomId}:lastActivity`;
-              await this.redisService.removeUserMessageIds(socketId, chatRoomId);
-              await this.redisService.leaveChatRoomById(chatRoomId, socketId);
-              await this.redisService.deleteChatRoomMessagesById(chatRoomId);
-              await this.redisService.deleteLastActiveTime(userKey, socketId);
-              await this.redisService.deleteLastActiveTime(chatRoomKey, socketId);
-            }
+            this.io
+              ?.of('/chat')
+              .to(chatRoom?.id)
+              .emit('inactive-chatRoom', chatRoom);
           }
         }
       }
@@ -232,6 +191,7 @@ export class ChatRoomManager {
           callback();
         } else {
           socket.emit('receive-chatRoom-session', null);
+          this.redisService.removeUserSessionId(socket.id);
           callback();
         }
       } catch (error) {
