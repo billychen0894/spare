ccd61a0c2ed1b83adb0b2b1ac816d944c68541ce
billychen0894 <billychen0894@gmail.com>
2024-05-19 23:55:45 -0700

    update: server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import { App } from '@/app';
 import { ChatRoute } from '@/routes/chats.route';
-import { ChatSocket } from '@/websocket/chat.socket';
 import { Websocket } from '@/websocket/websocket';
 import { createAdapter } from '@socket.io/redis-adapter';
 import { ValidateEnv } from '@utils/validateEnv';
 import { RedisClient } from './redisClient';
+import { ChatSocket } from './controllers/chatSocket.controller';
+import { ChatService } from './services/chat.service';
+import Container from 'typedi';
 
 ValidateEnv();
 
@@ -12,13 +14,14 @@ const app = new App([new ChatRoute()]);
 const httpServer = app.getHttpServer();
 const io = Websocket.getWebsocket(httpServer);
 const redisClient = RedisClient.getInstance();
+const chatService = Container.get(ChatService);
 
 io.adapter(createAdapter(redisClient.getPubClient(), redisClient.getSubClient()));
 
 io.initializeHandlers([
   {
     path: '/chat',
-    handler: new ChatSocket(),
+    handler: new ChatSocket(chatService, io),
   },
 ]);
 
